f632932c1849d93b596603631d8930e8678da5c6
Alex4512 <alexandrovich601@mail.ru>
2022-02-07 14:57:55 +0300

    whats?

--- lessons_python/test_Pytest_example.py
+++ lessons_python/test_Pytest_example.py
@@ -1,51 +1,59 @@
 from selenium import webdriver
 import time
-import unittest
+import pytest
 
 link1 = "http://suninjuly.github.io/registration1.html"
 link2 = "http://suninjuly.github.io/registration2.html"
+def test_text1():
 
-class TestAbs(unittest.TestCase):
-     def test_text1(self):
-         browser = webdriver.Chrome()
-         browser.get(link1)
-         input1 = browser.find_element_by_css_selector("input.form-control.first")
-         input1.send_keys('alex')
-         input2 = browser.find_element_by_css_selector("input[placeholder='Input your last name'")
-         input2.send_keys('yr')
-         input3 = browser.find_element_by_css_selector("input.form-control.third")
-         input3.send_keys('er@kk.ru')
-         time.sleep(10)
-          
-         button = browser.find_element_by_css_selector("button.btn")
-         button.click()
+    browser = webdriver.Chrome()
+    browser.get(link1)
 
-         time.sleep(1)
+    input1 = browser.find_element_by_css_selector("input.form-control.first")
+    input1.send_keys('alex')
+    input2 = browser.find_element_by_css_selector("input[placeholder='Input your last name'")
+    input2.send_keys('yr')
+    input3 = browser.find_element_by_css_selector("input.form-control.third")
+    input3.send_keys('er@kk.ru')
+ 
 
-         welcome_text_elt = browser.find_element_by_tag_name("h1")    
-         welcome_text = welcome_text_elt.text
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    time.sleep(1)
+
+    welcome_text_elt = browser.find_element_by_tag_name("h1")    
+    welcome_text = welcome_text_elt.text
     
-         self.assertEqual("Congratulations! You have successfully registered!" == welcome_text)
+    assert "Congratulations! You have successfully registered!" == welcome_text
+  
 
-         
-     def test_text1(self):
-         browser = webdriver.Chrome()
-         browser.get(link1)
-         input1 = browser.find_element_by_css_selector("input.form-control.first")
-         input1.send_keys('alex')
-         input2 = browser.find_element_by_css_selector("input[placeholder='Input your last name'")
-         input2.send_keys('yr')
-         input3 = browser.find_element_by_css_selector("input.form-control.third")
-         input3.send_keys('er@kk.ru')
-         time.sleep(10)
-         button = browser.find_element_by_css_selector("button.btn")
-         button.click()
-
-         time.sleep(1)
-
-         welcome_text_elt = browser.find_element_by_tag_name("h1")    
-         welcome_text = welcome_text_elt.text
+def test_text2():
     
-         self.assertEqual("Congratulations! You have successfully registered!" == welcome_text)
 
-if __name__ == "__main__": unittest.main()
+    browser = webdriver.Chrome()
+    browser.get(link2)
+
+    input1 = browser.find_element_by_css_selector("input.form-control.first")
+    input1.send_keys('alex')
+    input2 = browser.find_element_by_css_selector("input[placeholder='Input your last name'")
+    input2.send_keys('yr')
+    input3 = browser.find_element_by_css_selector("input.form-control.third")
+    input3.send_keys('er@kk.ru')
+    
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    time.sleep(1)
+
+    welcome_text_elt = browser.find_element_by_tag_name("h1")    
+    welcome_text = welcome_text_elt.text
+    
+    assert "Congratulations! You have successfully registered!" == welcome_text
+  
+
+
+
+if __name__ == "__main__":
+    pytest.main()
